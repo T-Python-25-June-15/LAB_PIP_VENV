59702532a17a1a16a67389ff2ffb42832331baf3
meshari alharbi <109522810+alharbisa24@users.noreply.github.com>
2025-06-25 15:14:19 +0300

    main

---
+++ main.py
@@ -0,0 +1,12 @@
+from art import *
+from colorama import Fore
+
+tprint("BELIEVE AND ACHEIVE", font="black")
+tprint("HELLO", font="sub-zero")
+
+tprint("TEST ", font="sweet")
+
+tprint("HI", font="knob")
+
+print(Fore.RED + 'red text')
+print(Fore.BLUE + 'blue text')
