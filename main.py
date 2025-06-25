16b439905a4f28f5ba8c1b290228d92983d84c9c
Your Name <your_email@example.com>
2025-06-26 01:18:39 +0300

    Lab day 8.2

---
+++ main.py
@@ -0,0 +1,19 @@
+from art import tprint
+
+from colorama import Fore, Style, init
+
+init()
+
+print(Fore.BLUE)
+tprint("BELIEVE AND ACHEIVE", font="block")
+
+print(Fore.GREEN)
+tprint("HELLO", font="sub-zero")
+
+print(Fore.RED)
+tprint("NEVER GIVE UP", font="crazy")
+
+print(Fore.YELLOW)
+tprint("SUCCESS", font="slant")
+
+print(Style.RESET_ALL)
