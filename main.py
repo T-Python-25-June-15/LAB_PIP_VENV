847399016633be1e8fe2cc3851df81e4cf8d0050
Turki20 <ttaiealmutairi@stu.kau.edu.sa>
2025-06-25 14:42:07 +0300

    Turki Ahmed

---
+++ main.py
@@ -0,0 +1,5 @@
+from art import *
+
+BELIEVE_AND_ACHEIVE = text2art("BELIEVE",font='block',chr_ignore=True) + "\n" + text2art("AND",font='block',chr_ignore=True) + "\n" + text2art("ACHEIVE",font='block',chr_ignore=True)
+print(BELIEVE_AND_ACHEIVE)
+
