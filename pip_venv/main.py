94ac2b218be5a73b8a793e1f26f0903c2c637393
Abdulrahman <a3.alsuhaymi@gmail.com>
2025-06-25 18:43:45 +0300

    PIP_Lab

---
+++ pip_venv/main.py
@@ -0,0 +1,8 @@
+from colorama import *
+from art import *
+tprint("BELIEVE AND ACHEIVE" ,font= "block")
+tprint("HELLO", font="sub_zero")
+aprint("hello" ,5 ,5)
+tprint("test",randart())
+print(Fore.CYAN + "hello world")
+print(Style.DIM + "hello world2")
