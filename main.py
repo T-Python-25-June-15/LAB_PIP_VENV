c45957ac54ec0f3a0c1889519a537af61046f2b2
lujain415 <lujainalaamri55@gmail.com>
2025-06-25 22:33:05 +0300

    completed

---
+++ main.py
@@ -0,0 +1,11 @@
+from art import *
+from colorama import *
+print(text2art("LUJAIN"))
+    
+print(Fore.BLUE + text2art("NEVER GIVE UP", font="block") + Style.RESET_ALL)
+print(text2art("BELIEVE AND ACHEIVE", font="block") )
+
+
+print(text2art("HELLO", font="sub-zero") )
+print(Fore.GREEN + text2art("HELLO ", font="sub-zero") + Style.RESET_ALL)
+print(Fore.MAGENTA + text2art("PEACE", font="epic") + Style.RESET_ALL)
